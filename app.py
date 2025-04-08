2b1ad298f2ebc5f0ea7427dd8d9cfadee0432e57
Sean Hsieh <seankh4444@gmail.com>
2025-04-07 19:40:38 -0700

    test 4 removed openai

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
-from openai import OpenAI  # Use the client class as shown in your example
 import os
 import util
 import db_config
